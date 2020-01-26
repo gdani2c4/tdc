b43c9eb47d284e757493b51e093e9e6b5c5230a7
Dani <atencion@danigil.com>
2020-01-26 02:27:27 +0000

    ya está programado el comando tdc -t

--- tdc.py
+++ tdc.py
@@ -1,4 +1,16 @@
-import sys, json, re
+import sys, json, re, os
+ficha_dat = {
+"js": [
+	{ "fa": '"',	"fc": '"' },
+	{ "fa": '\'',	"fc": '\'' },
+	{ "fa": '`',	"fc": '`' },
+	{ "fa": "/\*",	"fc": "\*/" , "sin_esc": 1 },
+	{ "fa": "//",	"fc": '\n' , "sin_esc": 1 } ],
+"py": [
+	{ "fa": '"',	"fc": '"' },
+	{ "fa": '\'',	"fc": '\'' },
+	{ "fa": '#',	"fc": '\n', "sin_esc": 1 } ]
+}
 msj_uso = (	"Uso: tdc [OPCIÓN] [ARCHIVO]...\n"
 			"traducir ARCHIVO(s) mediante la lista de "
 			"traducción $PWD/dat/tdc.json.\n"
@@ -21,11 +33,11 @@ def main():
 	as_argv( sys.argv[1:], rstdo, opc_x )
 	ach_prog_nomv = rstdo[ "ach_prog_nomv" ]
 	if opc_x["modo"] == "a":
-		act_tdc_dat( ach_prog_nomv )
+		act_dat( ach_prog_nomv )
 	elif opc_x["modo"] == "t":
 		tdc_achos_prog( ach_prog_nomv )
 
-def act_tdc_dat( ach_prog_nomv ):
+def act_dat( ach_prog_nomv ):
 	ach_prog = {}
 	tdcv_nv = { "cda": {} }
 	tdcv_ant = {}
@@ -36,27 +48,30 @@ def act_tdc_dat( ach_prog_nomv ):
 		ach_prog = { "ficha_v": [], "ach_cda": "" }
 		ach_prog_leer( ach_prog_nom, ach_prog )
 		as_ach_prog( ach_prog, tdcv_nv )
-	tdcv_ant_leer( rstdo )
-	tdcv_ant = rstdo["tdcv_ant"]
+	tdcv_leer( rstdo )
+	tdcv_ant = rstdo["tdc_v"]
 	tdcv_unir( tdcv_ant, tdcv_nv )
 	json.dumps( tdcv_nv, indent = 4 )
 	tdcv_escr( tdcv_nv, tdcv_ant )
 
 def tdc_achos_prog( ach_prog_nomv ):
-	pass
-
-ficha_dat = {
-"js": [
-	{ "fa": '"',	"fc": '"' },
-	{ "fa": '\'',	"fc": '\'' },
-	{ "fa": '`',	"fc": '`' },
-	{ "fa": "/\*",	"fc": "\*/" , "sin_esc": 1 },
-	{ "fa": "//",	"fc": '\n' , "sin_esc": 1 } ],
-"py": [
-	{ "fa": '"',	"fc": '"' },
-	{ "fa": '\'',	"fc": '\'' },
-	{ "fa": '#',	"fc": '\n', "sin_esc": 1 } ]
-}
+	cda_prog = ""
+	rstdo = {}
+	for ach_prog_nom in ach_prog_nomv:
+		rstdo = {}
+		tdcv_leer( rstdo )
+		tdc_v = rstdo[ "tdc_v" ]
+		print( tdc_v )
+		with open(ach_prog_nom, "r" ) as ach_prog:
+			cda_prog = ach_prog.read()
+			for ii in tdc_v["cda"]:
+				if not tdc_v["cda"][ ii ] == "":
+					cda_prog = cda_prog.replace( ii,
+						tdc_v["cda"][ii] )
+		for ii in sorted( tdc_v["abrv"] ):
+			cda_prog = cda_prog.replace( ii,
+				tdc_v["abrv"][ii] )
+		escr_ach_prog( ach_prog_nom, cda_prog )
 
 def ach_prog_leer( ach_nom, rstdo ):
 	rstdo["ficha_v"] = ficha_dat["js"]
@@ -84,13 +99,12 @@ def as_ach_prog( ach_prog, tdcv_nv ):
 				tdcv_nv["cda"][
 					ii.group( "fch%d" % kk ) ] = ""
 
-def tdcv_ant_leer( rstdo, tdc_nom_ach = "dat/tdc.json" ):
+def tdcv_leer( rstdo, tdc_nom_ach = "dat/tdc.json" ):
 	with open( tdc_nom_ach ) as ach_tdc:
 		try:
-			rstdo["tdcv_ant"] = json.load( ach_tdc )
+			rstdo["tdc_v"] = json.load( ach_tdc )
 		except FileNotFoundError:
-			print( err_ach_de_tdc_predet )
-			exit( -1 )
+			err( err_ach_de_tdc_predet )
 
 def tdcv_unir( tdcv_ant, tdcv_nv ):
 	tdcv_nv["abrv"] = {}
@@ -98,6 +112,17 @@ def tdcv_unir( tdcv_ant, tdcv_nv ):
 		if ii in tdcv_ant["cda"]:
 			tdcv_nv["cda"][ ii ] = tdcv_ant["cda"][ ii ]
 	tdcv_nv["abrv"] = tdcv_ant["abrv"]
+
+def escr_ach_prog( ach_prog_nom, cda_prog ):
+	( camino, ach_nom ) = os.path.split( ach_prog_nom )
+	ach_nom = os.path.join( camino, "EN", ach_nom )
+	try:
+		with open( ach_nom, "w" ) as ach_prog:
+			ach_prog.write( cda_prog )
+	except FileNotFoundError:
+		os.mkdir( os.path.join( camino, "EN" ),
+			mode = 0o755 )
+		escr_ach_prog( ach_prog_nom, cda_prog )
 
 def tdcv_escr( tdcv_nv, tdcv_ant,
 		tdc_nom_ach = "dat/tdc.json" ):
